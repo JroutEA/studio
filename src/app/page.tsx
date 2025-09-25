7c3d734f37dc8ebb29fcd1de4f32706befb06bd0
Jyoti Ranjan Rout <jrout@ea.com>
2025-09-25 13:42:10 +0000

    please remove the Yellow marked Area, Take the icon to the Green marked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { CharacterFinder } from '@/components/character-finder';
-import { Header } from '@/components/header';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
-      <Header />
       <main className="flex-1 container mx-auto py-8 lg:py-12">
         <CharacterFinder />
       </main>
