a9830179763d8b1b7dacd9ebccbd8d3ddbe7c1f1
Jyoti Ranjan Rout <jrout@ea.com>
2025-10-03 04:30:16 +0000

    Hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import { UnitFinder } from '@/components/unit-finder';
-import packageJson from '../../package.json';
+import fs from 'fs';
+import path from 'path';
 
 export default function Home() {
+  const packageJsonPath = path.join(process.cwd(), 'package.json');
+  const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8');
+  const packageJson = JSON.parse(packageJsonContent);
   const version = packageJson.version;
+
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <main className="flex-1 container mx-auto py-8 lg:py-12">
